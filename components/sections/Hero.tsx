ed2c24a51aa8ed2efe3395dfc2b611aa0c1dd7d6
Tristan Cherrier <tristan.cherrier@etu.univ-lehavre.fr>
2026-03-14 20:59:38 +0100

    Setup of next-intl for translation, moved app content to [locale], added en and fr translations (not fully, just to test), proxy added to redirect from / based on the locale

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,16 +1,18 @@
+import {useTranslations} from 'next-intl';
+
 /**
  * Hero Section: The first point of contact for visitors.
  * Simple Server Component as it doesn't require interactivity.
  */
 export default function Hero() {
+  const t = useTranslations('hero');
   return (
     <section id="about" className="py-15 text-center">
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-6xl text-foreground transition-colors">
         Hi, I'm <span className="text-primary">Tristan</span>
       </h1>
       <p className="mt-6 text-lg leading-8 text-muted max-w-2xl mx-auto transition-colors">
-        I'm a Fullstack Developer specialized in building robust web applications.
-        Check out my work below.
+        {t("sub")}
       </p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
         <a 
